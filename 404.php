091853fa027778309f24ce10f8be876b34ae6e2d
Matt Hopson <kayla@Mailmmc.com>
2014-03-01 22:16:50 -0600

    First commit: HTML5 Boilerplate base.

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php
+	$page = "Error";
+	$pageTitle = "Error 404 :: Page Not Found :: Icon Motosports";
+	$pageDescription = "";
+	require('includes/dspHeader.php');
+?>
+	<div class="page" id="error">
+		<h1>Whoops!</h1>
+		<p>This page has been moved or no longer exists. Why not check out the <a href="/">Home Page</a>?</p>
+	</div>
+
+<?php require('includes/dspFooter.php'); ?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	$page = "Home";
+	$pageTitle = "Icon Motosports :: Portland USA";
+	$pageDescription = "";
+	require('includes/dspHeader.php');
+?>
+
+
+<?php require('includes/dspFooter.php'); ?>
