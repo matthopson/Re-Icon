b2642dfa29faa2f0826fc684edb754939adcf36d
Matt Hopson <kayla@Mailmmc.com>
2014-03-02 21:06:50 -0600

    Footer done and responsive. Borrowed a small amount of CSS and DOM elements from original site.

--- includes/dspFooter.php
+++ includes/dspFooter.php
@@ -1,7 +1,137 @@
 		</div><!---container--->
-		<br /><br />
+		<br />
 		<footer>
-		
+			<div class="container">
+				<div class="contained">
+					<section id="dealer" class="quartered">				
+						<h2>Dealer Locator</h2>
+						<p>Icon does not sell any products directly online and we encourage you to support your local dealer. To find a dealer in your area, use our helpful dealer locator below.</p>
+						
+						<form action="/dealer-locator">
+							<input type="hidden" name="distance" value="20">
+							<div class="text-btn"><input class="txtFld" type="text" name="zipCode" placeholder="Enter US Zip Code"><input class="submitBtn" type="submit" value="GO"></div>
+						</form>
+						<br />
+						<a href="#">International Dealers</a>
+					</section>
+					<section id="twitter" class="quartered">
+						<h2>Twitter</h2>
+						<div id="twitterFeedPad">
+							<div id="twitterFeed" class="hover simple jspScrollable" tabindex="0" style="overflow: hidden; padding: 0px; width: 195px;">
+								<div class="jspContainer" style="width: 195px; height: 240px;">
+									<div class="jspPane" style="padding: 0px 3px 0px 0px; width: 178px; top: 0px;">
+										<div>
+											<span><a class="twt_user" href="http://twitter.com/V1R4S" target="blank">@V1R4S</a> did you submit your story to the website?</span>
+											<div class="twt_dt">Feb, 28</div>
+										</div>
+										<div class="post">
+											<span><a class="twt_user" href="http://twitter.com/knightabe" target="blank">@knightabe</a> it does, just make sure to submit your story on the website.</span>
+											<div class="twt_dt">Feb, 28</div>
+										</div>
+										<div class="post">
+											<span>RT <a class="twt_user" href="http://twitter.com/LossaLBC" target="blank">@LossaLBC</a>: <a class="twt_hash" href="http://twitter.com/search?q=%23fbf" target="blank">#fbf</a> me and @shilohghouse rocking @iconmotosports #gloves during @caferacertv shoot! The best gloves!!… <a class="twt_url" href="http://t.co/hL904N4xVX" target="blank">http://t.co/hL904N4xVX</a></span>
+											<div class="twt_dt">Feb, 28</div>
+										</div>
+										<div class="post">
+											<span><a class="twt_user" href="http://twitter.com/Silodrome" target="blank">@Silodrome</a> checks out the latest pair of @IconOneThousand <a class="twt_hash" href="http://twitter.com/search?q=%23kevlar" target="blank">#kevlar</a> #jeans <a class="twt_url" href="http://t.co/T6981RT3ze" target="blank">http://t.co/T6981RT3ze</a></span>
+											<div class="twt_dt">Feb, 28</div>
+										</div>
+										<div class="post">
+											<span>RT <a class="twt_user" href="http://twitter.com/Silodrome" target="blank">@Silodrome</a>: The @iconmotosports Rouser Kevlar Jeans- Today on Silodrome. <a class="twt_hash" href="http://twitter.com/search?q=%23jeans" target="blank">#jeans</a> #denim #kevlar #motorcycles <a class="twt_url" href="http://t.co/NHXx9Ftqsy" target="blank">http://t.co/NHXx9Ftqsy</a></span>
+											<div class="twt_dt">Feb, 28</div>
+										</div>
+										<div class="post">
+											<span><a class="twt_user" href="http://twitter.com/Petrolblahblah" target="blank">@Petrolblahblah</a> that's the D3O impact protectors inside ; )</span>
+											<div class="twt_dt">Feb, 26</div>
+										</div>
+										<div class="post">
+											<span>RT <a class="twt_user" href="http://twitter.com/MotoUSA" target="blank">@MotoUSA</a>: Creativity never stops around Slabtown. @IconMotosports @TriumphAmerica Custom Build: ICON Triumph Speedmaster. <a class="twt_url" href="http://t.co/jQ" target="blank">http://t.co/jQ</a>…</span>
+											<div class="twt_dt">Feb, 25</div>
+										</div>
+										<div class="post">
+											<span>RT <a class="twt_user" href="http://twitter.com/Sara_LaShea" target="blank">@Sara_LaShea</a>: I think it's time for a new <a class="twt_hash" href="http://twitter.com/search?q=%23icon" target="blank">#icon</a> #helmet ������ @iconmotosports <a class="twt_url" href="http://t.co/2dCI1aLXXd" target="blank">http://t.co/2dCI1aLXXd</a></span>
+											<div class="twt_dt">Feb, 23</div>
+										</div>
+										<div class="post">
+											<span>Holeshots or bust <a class="twt_hash" href="http://twitter.com/search?q=%23AtlantaSX" target="blank">#AtlantaSX</a> #sxonfox <a class="twt_url" href="http://t.co/LESGNvoxob" target="blank">http://t.co/LESGNvoxob</a></span>
+											<div class="twt_dt">Feb, 22</div>
+										</div>
+										<div class="post">
+											<span>Villopoto making an entrance. The king has arrived <a class="twt_hash" href="http://twitter.com/search?q=%23SXonFOX" target="blank">#SXonFOX</a> #hawtlanta <a class="twt_user" href="http://twitter.com/thormxofficial" target="blank">@thormxofficial</a> #makeitfour</span>
+											<div class="twt_dt">Feb, 22</div>
+										</div>
+									</div>
+									<div class="jspVerticalBar">
+										<div class="jspCap jspCapTop"></div>
+										<a class="jspArrow jspArrowUp jspDisabled"></a>
+										<div class="jspTrack" style="height: 240px;">
+											<div class="jspDrag" style="height: 68px;">
+												<div class="jspDragTop"></div>
+												<div class="jspDragBottom"></div>
+											</div>
+										</div>
+										<a class="jspArrow jspArrowDown"></a>
+										<div class="jspCap jspCapBottom"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+					<section id="social" class="quartered">
+						<h2>Follow Us On</h2>
+						<div class="followUs">
+							<span>
+		                        <a target="_blank" class="icon1000" title="ICON1000" href="http://icon1000.com/culture/blog/"></a>
+								<a target="_blank" class="facebook" title="Facebook" href="http://www.facebook.com/pages/Icon/246334089439"></a>
+								<a target="_blank" class="twitter" title="Twitter" href="http://twitter.com/IconMotosports"></a>
+								<a target="_blank" class="youTube" title="YouTube" href="http://www.youtube.com/iconmotosports"></a>
+								</span>
+							<span>
+								<a target="_blank" class="pinterest" title="Pinterest" href="https://pinterest.com/iconmotosports/"></a>
+								<a target="_blank" class="google" title="Google+" href="https://plus.google.com/110965041137193952156" rel="publisher"></a>
+							</span>
+							<div class="clear"></div>
+						</div>
+						<h2>Email Signup</h2>
+						<p>To keep up with the latest ICON news, and product releases, simply sign up for our mailing list below. We will do our best to keep you in the know.</p>
+						<form id="newsletterForm" action="">
+							<div class="text-btn">
+								<input id="newsletterEmail" class="txtFld" type="text" name="email" placeholder="Enter Email Address Here"><input class="submitBtn" id="newsletterSubmit" type="submit" value="GO"><img src="http://assets-static.lemansnet.com/skins/sam/wait.gif" id="newsletterLoading">
+							</div>
+							<div style="display:none;">
+								<label><input type="radio" name="action" checked="true" value="subscribe">Subscribe</label>
+								<label><input type="radio" name="action" value="unsubscribe">Unsubscribe</label>
+							</div>
+							<div id="newsletterMsg"></div>
+						</form>
+					</section>
+					<section id="links" class="quartered">
+						<h2>Links</h2>
+						<nav>
+							<ul>
+								<li><a href="#">Limiter Request</a></li>
+								<li><a href="#">Catalog Download</a></li>
+								<li><a href="#">Shield Change Videos</a></li>
+								<li><a href="#">Size Charts</a></li>
+								<li><a href="#">Careers</a></li>
+							</ul>
+							<ul>
+								<li><a href="#">Warranty Info</a></li>
+								<li><a href="#">Terms & Conditions</a></li>
+								<li><a href="#">Find A Dealer</a></li>
+								<li><a href="#">Sticker Request</a></li>
+							</ul>
+							<ul>
+								<li><a href="#">Washing Instructions</a></li>
+								<li><a href="#">Helmet Care</a></li>
+								<li><a href="#">Become A Dealer</a></li>
+								<li><a href="#">Busted and Broken</a></li>
+							</ul>
+						</nav>
+					</section>
+					<div class="clear"></div>
+				</div>
+			</div>
 		</footer>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
